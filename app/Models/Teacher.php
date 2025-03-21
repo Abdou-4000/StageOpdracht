67feb6fad412c4442c1767b006c771c534e54145
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-21 14:05:17 +0100

    Made Teacher, Cities and Category Models, added eloquent relations

---
+++ app/Models/Teacher.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    public function city() {
+        return $this->belongsTo(City::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+}
