07c9055af69f2601b6b7c1289c317052e2fabd3a
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-24 15:33:32 +0100

    Added basic styling header/teacher index

--- resources/views/teachers/index.blade.php
+++ resources/views/teachers/index.blade.php
@@ -1,37 +1,52 @@
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-    {{ __('Teachers') }} 
-</h2>
+@extends('layout')
 
-<div class="p-6 text-gray-900">
-    <a href="{{ route('teachers.create') }}">Add new teacher</a> 
-    <table> 
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($teachers as $teacher)
+@section('content')
+<div class="container mx-auto p-6">
+    <div class="overflow-x-auto">
+        <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+            <thead class="bg-gray-800 text-white">
                 <tr>
-                    <td>{{ $teacher->firstname }}</td>
-                    <td>{{ $teacher->lastname }}</td>
-                    <td>{{ $teacher->companyname }}</td>
-                    <td>{{ $teacher->city->name}}</td>
-                    @foreach ($teacher->category as $category)
-                        <td>{{$category->name}}</td>
-                    @endforeach
-                    <td>
-                        <a href="{{ route('teachers.edit', $teacher) }}">Edit</a>
-                        <form method="POST" action="{{ route('teachers.destroy', $teacher) }}"> 
-                            @csrf
-                            @method('DELETE')
-
-                            <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    </td>
+                    <th class="py-3 px-6 text-left">Naam</th>
+                    <th class="py-3 px-6 text-left"></th>
+                    <th class="py-3 px-6 text-left">Email</th>
+                    <th class="py-3 px-6 text-left">Telefoon</th>
+                    <th class="py-3 px-6 text-left">Bedrijf</th>
+                    <th class="py-3 px-6 text-left"></th>
+                    <th class="py-3 px-6 text-left">Adres</th>
+                    <th class="py-3 px-6 text-left">Categorieën</th>
+                    <th class="py-3 px-6 text-left">Acties</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table> 
+            </thead>
+            <tbody class="divide-y divide-gray-200 text-black">
+                @foreach($teachers as $teacher)
+                    <tr class="hover:bg-gray-100">
+                        <td class="py-4 px-6">{{ $teacher->firstname }}</td>
+                        <td class="py-4 px-6">{{ $teacher->lastname }}</td>
+                        <td class="py-4 px-6">{{ $teacher->email }}</td>
+                        <td class="py-4 px-6">{{ $teacher->phone }}</td>
+                        <td class="py-4 px-6">{{ $teacher->companyname }}</td>
+                        <td class="py-4 px-6">{{ $teacher->companynumber }}</td>
+                        <td class="py-4 px-6">{{ $teacher->street }} {{ $teacher->streetnumber }}, {{ $teacher->city->name }}</td>
+                        <td class="py-4 px-6">
+                            @foreach ($teacher->category as $category)
+                                <span class="bg-blue-200 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                                    {{$category->name}}
+                                </span>
+                            @endforeach
+                        </td>
+                        <td class="py-4 px-6 flex space-x-2">
+                            <a href="{{ route('teachers.edit', $teacher) }}" class="text-blue-600 hover:underline">Edit</a>
+                            <form method="POST" action="{{ route('teachers.destroy', $teacher) }}" onsubmit="return confirm('Are you sure?')">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-red-600 hover:underline">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
+
+@endsection
