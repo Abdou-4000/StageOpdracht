af464d2b51b65128f18d1b8238f83697fa6444ac
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-25 14:17:11 +0100

    Added requirments/error messages update form

--- resources/views/teachers/edit.blade.php
+++ resources/views/teachers/edit.blade.php
@@ -5,33 +5,74 @@
 
         <div> 
             <div>
-                <label for="firstname">Firstname:</label>
+                <input type="text" name="firstname" id="firstname" value="{{ $teacher->firstname }}">
+                @error('firstname')
+                    <div class="error">{{ $message }}</div>
+                @enderror
             </div>
-            <input type="text" name="firstname" id="firstname" value="{{ $teacher->firstname }}">
-            <input type="text" name="lastname" id="lastname" value="{{ $teacher->lastname }}">
-            <input type="email" name="email" id="email" value="{{ $teacher->email }}">
-            <input type="number" name="phone" id="phone" value="{{ $teacher->phone }}">
-            <input type="number" name="companynumber" id="companynumber" value="{{ $teacher->companynumber }}">
-            <input type="text" name="companyname" id="companyname" value="{{ $teacher->companyname }}">
-            <input type="text" name="street" id="street" value="{{ $teacher->street }}">
-            <input type="number" name="streetnumber" id="streetnumber" value="{{ $teacher->streetnumber }}">
-            <input type="text" name="city_name" id="city_name" value="{{ $teacher->city->name }}">
-             @foreach ($categories as $category)
-                <label>
-                    <input type="checkbox" value="{{$category->id}}" name="categories[]"
-                    @if ($teacher->category->contains($category->id)) 
-                        checked 
-                    @endif>
-                    {{$category->name}}
-                </label>
-            @endforeach
-            @error('city_name')
-                <p>{{$message}}</p>
-            @enderror
+            <div>
+                <input type="text" name="lastname" id="lastname" value="{{ $teacher->lastname }}">
+                @error('lastname')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="email" name="email" id="email" value="{{ $teacher->email }}">
+                @error('email')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="number" name="phone" id="phone" value="{{ $teacher->phone }}">
+                @error('phone')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="number" name="companynumber" id="companynumber" value="{{ $teacher->companynumber }}">
+                @error('companynumber')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="text" name="companyname" id="companyname" value="{{ $teacher->companyname }}">
+                @error('companyname')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="text" name="street" id="street" value="{{ $teacher->street }}">
+                @error('street')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="number" name="streetnumber" id="streetnumber" value="{{ $teacher->streetnumber }}">
+                @error('streetnumber')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="text" name="city_name" id="city_name" value="{{ $teacher->city->name }}">
+                @error('city_name')
+                    <p>{{$message}}</p>
+                @enderror
+            </div>
+            <div>
+                @foreach ($categories as $category)
+                    <label>
+                        <input type="checkbox" value="{{$category->id}}" name="categories[]"
+                        @if ($teacher->category->contains($category->id)) 
+                            checked 
+                        @endif>
+                        {{$category->name}}
+                    </label>
+                @endforeach
+            </div>
         </div>
         <div>
             <button type="submit">
-                Save
+                Opslaan
             </button>
         </div> 
     </form>
