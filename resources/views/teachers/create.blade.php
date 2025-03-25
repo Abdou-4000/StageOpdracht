70231ec0a315f3daa7ca79000788507fa76db0c6
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-25 14:58:20 +0100

    Changed the edit/create forms to have a general layout

--- resources/views/teachers/create.blade.php
+++ resources/views/teachers/create.blade.php
@@ -1,75 +1,13 @@
-<div class="p-6 text-gray-900">
-    <form method="POST" action="{{ route('teachers.store') }}"> 
-        @csrf
+@extends('layout')
 
-        <div>
-            <div>
-                <input type="text" name="firstname" id="firstname" placeholder="first name">
-                @error('firstname')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="text" name="lastname" id="lastname" placeholder="last name">
-                @error('lastname')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="email" name="email" id="email" placeholder="email">
-                @error('email')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="number" name="phone" id="phone" placeholder="phone">
-                @error('phone')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="number" name="companynumber" id="companynumber" placeholder="company number">
-                @error('companynumber')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="text" name="companyname" id="companyname" placeholder="company name">
-                @error('companyname')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="text" name="street" id="street" placeholder="street">
-                @error('street')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div> 
-            <div>
-                <input type="number" name="streetnumber" id="streetnumber" placeholder="street number">
-                @error('streetnumber')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="text" name="city_name" id="city_name" placeholder="city">
-                @error('city_name')
-                    <p>{{$message}}</p>
-                @enderror
-            </div>
-            <div>
-                @foreach ($categories as $category)
-                    <label>
-                        <input type="checkbox" value="{{$category->id}}" name="categories[]">
-                        {{$category->name}}
-                    </label>
-                @endforeach
-            </div>
-        </div>
-        <div>
-            <button type="submit">
-                Opslaan
-            </button>
-        </div> 
-    </form>
-</div>
+@section('content')
+    <div class="p-6 text-gray-900">
+        <h1>Create Teacher</h1>
+        @include('teachers._form', [
+            'action' => route('teachers.store'),
+            'isEdit' => false,
+            'teacher' => null,
+            'categories' => $categories
+        ])
+    </div>
+@endsection
--- resources/views/teachers/edit.blade.php
+++ resources/views/teachers/edit.blade.php
@@ -1,79 +1,13 @@
-<div class="p-6 text-gray-900">
-    <form method="POST" action="{{ route('teachers.update', $teacher) }}"> 
-        @csrf
-        @method('PUT')
+@extends('layout')
 
-        <div> 
-            <div>
-                <input type="text" name="firstname" id="firstname" value="{{ $teacher->firstname }}">
-                @error('firstname')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="text" name="lastname" id="lastname" value="{{ $teacher->lastname }}">
-                @error('lastname')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="email" name="email" id="email" value="{{ $teacher->email }}">
-                @error('email')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="number" name="phone" id="phone" value="{{ $teacher->phone }}">
-                @error('phone')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="number" name="companynumber" id="companynumber" value="{{ $teacher->companynumber }}">
-                @error('companynumber')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="text" name="companyname" id="companyname" value="{{ $teacher->companyname }}">
-                @error('companyname')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="text" name="street" id="street" value="{{ $teacher->street }}">
-                @error('street')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="number" name="streetnumber" id="streetnumber" value="{{ $teacher->streetnumber }}">
-                @error('streetnumber')
-                    <div class="error">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <input type="text" name="city_name" id="city_name" value="{{ $teacher->city->name }}">
-                @error('city_name')
-                    <p>{{$message}}</p>
-                @enderror
-            </div>
-            <div>
-                @foreach ($categories as $category)
-                    <label>
-                        <input type="checkbox" value="{{$category->id}}" name="categories[]"
-                        @if ($teacher->category->contains($category->id)) 
-                            checked 
-                        @endif>
-                        {{$category->name}}
-                    </label>
-                @endforeach
-            </div>
-        </div>
-        <div>
-            <button type="submit">
-                Opslaan
-            </button>
-        </div> 
-    </form>
-</div>
+@section('content')
+    <div class="p-6 text-gray-900">
+        <h1>Edit Teacher</h1>
+        @include('teachers._form', [
+            'action' => route('teachers.update', $teacher->id),
+            'isEdit' => true,
+            'teacher' => $teacher,
+            'categories' => $categories
+        ])
+    </div>
+@endsection
