fda29d6941cbba78a8216a4cd672cf39aa958bef
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-25 14:22:54 +0100

    Added error messages to create form

--- resources/views/teachers/create.blade.php
+++ resources/views/teachers/create.blade.php
@@ -2,29 +2,73 @@
     <form method="POST" action="{{ route('teachers.store') }}"> 
         @csrf
 
-        <div> 
-            <input type="text" name="firstname" id="firstname" placeholder="first name">
-            <input type="text" name="lastname" id="lastname" placeholder="last name">
-            <input type="email" name="email" id="email" placeholder="email">
-            <input type="number" name="phone" id="phone" placeholder="phone">
-            <input type="number" name="companynumber" id="companynumber" placeholder="company number">
-            <input type="text" name="companyname" id="companyname" placeholder="company name">
-            <input type="text" name="street" id="street" placeholder="street">
-            <input type="number" name="streetnumber" id="streetnumber" placeholder="street number">
-            <input type="text" name="city_name" id="city_name" placeholder="city">
-            @foreach ($categories as $category)
-                <label>
-                    <input type="checkbox" value="{{$category->id}}" name="categories[]">
-                    {{$category->name}}
-                </label>
-            @endforeach
-            @error('city_name')
-                <p>{{$message}}</p>
-            @enderror
+        <div>
+            <div>
+                <input type="text" name="firstname" id="firstname" placeholder="first name">
+                @error('firstname')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="text" name="lastname" id="lastname" placeholder="last name">
+                @error('lastname')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="email" name="email" id="email" placeholder="email">
+                @error('email')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="number" name="phone" id="phone" placeholder="phone">
+                @error('phone')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="number" name="companynumber" id="companynumber" placeholder="company number">
+                @error('companynumber')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="text" name="companyname" id="companyname" placeholder="company name">
+                @error('companyname')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="text" name="street" id="street" placeholder="street">
+                @error('street')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div> 
+            <div>
+                <input type="number" name="streetnumber" id="streetnumber" placeholder="street number">
+                @error('streetnumber')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <input type="text" name="city_name" id="city_name" placeholder="city">
+                @error('city_name')
+                    <p>{{$message}}</p>
+                @enderror
+            </div>
+            <div>
+                @foreach ($categories as $category)
+                    <label>
+                        <input type="checkbox" value="{{$category->id}}" name="categories[]">
+                        {{$category->name}}
+                    </label>
+                @endforeach
+            </div>
         </div>
         <div>
             <button type="submit">
-                Save
+                Opslaan
             </button>
         </div> 
     </form>
