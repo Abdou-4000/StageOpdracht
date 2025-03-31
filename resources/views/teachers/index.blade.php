7b1230a9c6ee537238025bb55adbeb8315b98826
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-31 13:01:29 +0200

    Changed coordinates view in table

--- resources/views/teachers/index.blade.php
+++ resources/views/teachers/index.blade.php
@@ -39,7 +39,13 @@
                             <td class="py-4 px-6">{{ $teacher->companyname }}</td>
                             <td class="py-4 px-6">{{ $teacher->companynumber }}</td>
                             <td class="py-4 px-6">{{ $teacher->street }} {{ $teacher->streetnumber }}, {{ $teacher->city->name }}</td>
-                            <td class="py-4 px-6">{{ $teacher->lat }} - {{ $teacher->lng }}</td>
+                            <td class="flex  justify-center py-4 px-6">
+                                @if (isset($teacher->lat) && isset($teacher->lng))
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16"> <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/> </svg>
+                                @else
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/> </svg>
+                                @endif
+                            </td>
                             <td class="py-4 px-6">
                                 @foreach ($teacher->category as $category)
                                     <span class="flex justify-center bg-accentBlue text-gray-light text-xs font-semibold px-2.5 py-0.5 rounded-lg block mt-1">
