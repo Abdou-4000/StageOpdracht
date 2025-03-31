59dd32de1d53618073f22a482bf3d467b05e0db2
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-31 12:46:18 +0200

    Fixed menu

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,9 +4,18 @@
 
 <div class="flex flex-wrap flex-row justify-between bg-white sticky top-0">
     <img class="flex w-64 pt-8 pl-8 bg-white" src="{{ asset('assets/Logo.png') }}" alt="Logo">
-
-    <div class="flex m-8 mt-16">
-        <a class="flex text-white bg-red w-96 h-10 p-2 justify-center rounded-full" href="{{ route('teachers.index') }}">
+    <div>naam</div>
+    <div class="mt-[80px]">
+        <a class="text-black" href="{{ route('teachers.index')}}">Leerkrachtenoverzicht</a>
+    </div>
+    <div class="mt-[80px]">
+        <a class="text-black" href="{{ route('teachers.create')}}">Leerkracht toevoegen</a>
+    </div>
+    <div class="mt-[80px]">
+        <a class="text-black" href="{{ route('categories.index')}}">Categorieënbeheer</a>
+    </div>
+    <div class="flex m-8 mt-[72px]">
+        <a class="flex text-white bg-red w-96 h-10 p-2 justify-center rounded-full" href="{{ route('map') }}">
             Kaart
         </a> 
     </div>
