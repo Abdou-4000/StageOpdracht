4a955be62c9d5695a3f47092960f13bdfe8259eb
carinezoe <zoecleo.dreessen@gmail.com>
2025-03-31 15:26:52 +0200

    Added underline in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -15,13 +15,13 @@
     <img class="flex w-64 pt-8 pl-8 bg-white" src="{{ asset('assets/Logo.png') }}" alt="Logo">
     <div>naam</div>
     <div class="mt-[80px]">
-        <a class="text-black" href="{{ route('teachers.index')}}">Leerkrachtenoverzicht</a>
+        <a class="text-black {{ request()->routeIs('teachers.index') ? 'active' : '' }}" href="{{ route('teachers.index')}}">Leerkrachtenoverzicht</a>
     </div>
     <div class="mt-[80px]">
-        <a class="text-black" href="{{ route('teachers.create')}}">Leerkracht toevoegen</a>
+        <a class="text-black {{ request()->routeIs('teachers.create') ? 'active' : '' }}" href="{{ route('teachers.create')}}">Leerkracht toevoegen</a>
     </div>
     <div class="mt-[80px]">
-        <a class="text-black" href="{{ route('categories.index')}}">Categorieënbeheer</a>
+        <a class="text-black {{ request()->routeIs('categories.index') ? 'active' : '' }}" href="{{ route('categories.index')}}">Categorieënbeheer</a>
     </div>
     <div class="flex m-8 mt-[72px]">
         <a class="flex text-white bg-red w-96 h-10 p-2 justify-center rounded-full" href="{{ route('map') }}">
