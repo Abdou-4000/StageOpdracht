4acc2ef60e764126820ac2acdaec55319494c9ca
carinezoe <zoecleo.dreessen@gmail.com>
2025-04-15 15:31:21 +0200

    Added buttons to show only one calendar at any given time

--- resources/js/pages/Agenda.vue
+++ resources/js/pages/Agenda.vue
@@ -1,11 +1,36 @@
 <script setup>
+import { ref } from 'vue'
 
+const agendaWeek = ref(false);
+const agendaMonth = ref(true);
+
+function showMonth () {
+    agendaWeek.value = false;
+    agendaMonth.value = true;
+}
+
+function showWeek () {
+    agendaWeek.value = true;
+    agendaMonth.value = false;
+}
 
 </script>
 
 <template>
+    <div class="flex justify-between p-4">
+        <button class="text-gray-dark" @click="showMonth">
+            month
+        </button>
+        <button class="text-gray-dark" @click="showWeek">
+            week
+        </button>
+    </div>
     <div class="flex flex-wrap flex-row gap-32">
-        <AgendaWeek/>
-        <AgendaMonth/>
+        <div v-if="agendaWeek">
+            <AgendaWeek/>
+        </div>
+        <div v-if="agendaMonth"> 
+            <AgendaMonth/>
+        </div>
     </div>
 </template>
